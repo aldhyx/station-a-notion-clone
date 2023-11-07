270340e189a7991f6e47f92537f2ef7cbf7273a2
fsaythanry <aldyzkobf@gmail.com>
2023-11-08 00:51:01 +0800

    setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: 'tailwind.config.ts',
+  tailwindFunctions: ['clsx'],
+  printWidth: 90,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  quoteProps: 'as-needed',
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+}
