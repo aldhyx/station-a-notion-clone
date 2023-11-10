474b4eb02a0b8859f5d42b09b31f249777e6d045
fsaythanry <aldyzkobf@gmail.com>
2023-11-10 19:53:33 +0800

    add use scroll top hook

---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export default function useScrollTop(threshold = 10) {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > threshold) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [threshold])
+
+  return scrolled
+}
