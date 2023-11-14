981642ccf4758afa3c845b217c938597e3e4780e
fsaythanry <aldyzkobf@gmail.com>
2023-11-14 14:24:56 +0800

    build getting started page

---
+++ app/(main)/docs/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+export default function DocumentsPage() {
+  return (
+    <div className="flex h-[calc(100vh-48px)] flex-col items-center justify-center">
+      <Image
+        alt=""
+        src="/assets/heroes.png"
+        width={600}
+        height={700}
+        className="mb-5 object-contain grayscale"
+      />
+      <p className="mb-5">Create new page to start write your idea.</p>
+      <Button variant="zinc" className="rounded-sm">
+        Create New Page
+      </Button>
+    </div>
+  )
+}
