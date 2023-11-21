f7cab3861f316935ea794b61cdbb4cf36331c183
fsaythanry <aldyzkobf@gmail.com>
2023-11-22 00:40:31 +0800

    modify error block component

--- components/error-block.tsx
+++ components/error-block.tsx
@@ -4,7 +4,7 @@ export default function ErrorBlock({ message }: { message?: string }) {
   if (!message || typeof message !== "string") return null
 
   return (
-    <p className="flex items-start py-3 text-sm leading-none text-red-600">
+    <p className="flex items-start text-sm leading-none text-red-600">
       <AlertCircleIcon className="mr-2 h-4 w-4 shrink-0" />
       {message}
     </p>
