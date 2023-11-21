51387c35002c8970920f7d4b52ad9b94fde17c0f
fsaythanry <aldyzkobf@gmail.com>
2023-11-21 15:07:36 +0800

    fix error block component

--- components/error-block.tsx
+++ components/error-block.tsx
@@ -4,7 +4,7 @@ export default function ErrorBlock({ message }: { message?: string }) {
   if (!message || typeof message !== "string") return null
 
   return (
-    <p className="mt-4 flex items-start text-sm leading-normal text-red-600">
+    <p className="flex items-start py-3 text-sm leading-normal text-red-600">
       <AlertCircleIcon className="mr-2 h-4 w-4" />
       {message}
     </p>
