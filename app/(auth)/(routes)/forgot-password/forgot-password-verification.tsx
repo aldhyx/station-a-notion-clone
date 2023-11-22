b081fa9186ded8c24c208838481f9c855fe3dcf9
fsaythanry <aldyzkobf@gmail.com>
2023-11-23 02:19:56 +0800

    build forgot password page

---
+++ app/(auth)/(routes)/forgot-password/forgot-password-verification.tsx
@@ -0,0 +1,127 @@
+"use client"
+
+import ErrorBlock from "@/components/error-block"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { getApiError } from "@/lib/error/api-error"
+import { supabase } from "@/lib/supabase/client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { LoaderIcon, MailCheckIcon } from "lucide-react"
+import Link from "next/link"
+import { redirect, useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const verificationSchema = z.object({
+  code: z.string({ required_error: "Invalid verification code" }).min(4, {
+    message: "Invalid verification code",
+  }),
+})
+
+type Props = { email: string | undefined }
+
+export default function ForgotPasswordVerificationPage({ email }: Props) {
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof verificationSchema>>({
+    resolver: zodResolver(verificationSchema),
+  })
+
+  const submitHandler = form.handleSubmit(async ({ code }) => {
+    try {
+      const { data, error } = await supabase.auth.verifyOtp({
+        email: email!,
+        token: code,
+        type: "recovery",
+      })
+
+      if (error) {
+        // rate limit error
+        if (error.status === 429) throw new Error("")
+        throw new Error(error.message)
+      }
+
+      router.replace("/reset-password")
+    } catch (error) {
+      form.setError("root.apiError", { message: getApiError(error) })
+    }
+  })
+
+  const {
+    formState: { isSubmitting, isValid, errors },
+  } = form
+
+  const isDisableSubmit = !isValid || isSubmitting
+  const isLoadingSubmit = isSubmitting
+
+  if (!email) return redirect("/forgot-password")
+
+  return (
+    <>
+      <MailCheckIcon className="mb-4 h-20 w-20" />
+
+      <h1 className="mb-2 text-2xl font-bold md:text-3xl">Check your email</h1>
+      <p className="mb-8 w-full text-center text-sm">
+        We have sent the verification code to <strong>{email}</strong>. Please, enter the
+        code down below to reset your password.
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={submitHandler}
+          className="flex w-full flex-col gap-y-3"
+          autoComplete="off"
+        >
+          <FormField
+            control={form.control}
+            name="code"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Enter the verification code..."
+                    type="text"
+                    inputSize="lg"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            size="lg"
+            className="w-full"
+            variant="outline-blue"
+            type="submit"
+            disabled={isDisableSubmit}
+          >
+            {isLoadingSubmit && (
+              <LoaderIcon className="animate mr-2 h-4 w-4 animate-spin" />
+            )}
+            Continue to Reset Password
+          </Button>
+
+          <ErrorBlock message={errors.root?.apiError.message} />
+        </form>
+      </Form>
+
+      <hr className="my-8 w-full border-zinc-200" />
+
+      <div className="w-full text-left text-sm">
+        <p className="mb-2 font-medium">Didn&apos;t receive email? </p>
+        <ol className="list-inside list-disc">
+          <li>Check your spam or junk folder.</li>
+          <li>
+            <Button variant="link-blue" className="h-auto p-0 font-normal" asChild>
+              <Link href="/forgot-password">Click here</Link>
+            </Button>{" "}
+            to try another email address
+          </li>
+        </ol>
+      </div>
+    </>
+  )
+}
---
+++ app/(auth)/(routes)/forgot-password/forgot-password.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import ErrorBlock from "@/components/error-block"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { getApiError } from "@/lib/error/api-error"
+import { supabase } from "@/lib/supabase/client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { LoaderIcon } from "lucide-react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { Dispatch, SetStateAction } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const forgotPasswordSchema = z.object({
+  email: z
+    .string({
+      required_error: "Invalid email address",
+    })
+    .email({ message: "Invalid email address" })
+    .toLowerCase(),
+})
+
+type Props = {
+  setEmail: Dispatch<SetStateAction<string | undefined>>
+}
+export default function ForgotPasswordPage({ setEmail }: Props) {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof forgotPasswordSchema>>({
+    resolver: zodResolver(forgotPasswordSchema),
+  })
+
+  const submitHandler = form.handleSubmit(async ({ email }) => {
+    try {
+      const { error } = await supabase.auth.resetPasswordForEmail(email)
+
+      if (error) throw new Error(error.message)
+
+      setEmail(email)
+      router.push("/forgot-password?steps=2")
+    } catch (error) {
+      form.setError("root.apiError", { message: getApiError(error) })
+    }
+  })
+
+  const {
+    formState: { isSubmitting, errors },
+    watch,
+  } = form
+
+  const currentFormState = watch()
+  const isDisableSubmit = isSubmitting || !currentFormState.email
+  const isLoadingSubmit = isSubmitting
+
+  return (
+    <>
+      <h1 className="mb-8 text-3xl font-bold md:text-4xl">Forgot password</h1>
+      <p className="mb-3 w-full text-left text-sm">
+        Enter your email address associated with your account, and we&apos;ll send you a
+        link to reset your password.
+      </p>
+
+      <Form {...form}>
+        <form
+          onSubmit={submitHandler}
+          className="flex w-full flex-col gap-y-3"
+          autoComplete="off"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your email address..."
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            size="lg"
+            className="w-full"
+            variant="outline-blue"
+            type="submit"
+            disabled={isDisableSubmit}
+          >
+            {isLoadingSubmit && (
+              <LoaderIcon className="animate mr-2 h-4 w-4 animate-spin" />
+            )}
+            Send reset password link
+          </Button>
+
+          <ErrorBlock message={errors.root?.apiError.message} />
+
+          <hr className="my-8 w-full border-zinc-200" />
+
+          <div className="mt-0 flex justify-center gap-x-1 text-sm">
+            <p className="text-zinc-700">Don&apos;t have an account?</p>
+
+            <Button variant="link-blue" className="h-auto p-0 font-normal" asChild>
+              <Link href="/signup">Sign up here</Link>
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  )
+}
