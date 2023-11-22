fad487693eca2ae7fa18df1e130fbf8d0a870d64
fsaythanry <aldyzkobf@gmail.com>
2023-11-23 02:18:49 +0800

    modify middleware

--- middleware.ts
+++ middleware.ts
@@ -8,14 +8,19 @@ export async function middleware(request: NextRequest) {
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
   const { data, error } = await supabase.auth.getSession()
 
-  if (!data.session && request.nextUrl.pathname.startsWith("/docs")) {
+  if (
+    !data.session &&
+    (request.nextUrl.pathname.startsWith("/docs") ||
+      request.nextUrl.pathname.startsWith("/reset-password"))
+  ) {
     return NextResponse.redirect(new URL("/login", request.url))
   }
 
   if (
     data.session &&
     (request.nextUrl.pathname.startsWith("/login") ||
-      request.nextUrl.pathname.startsWith("/signup"))
+      request.nextUrl.pathname.startsWith("/signup") ||
+      request.nextUrl.pathname.startsWith("/forgot-password"))
   ) {
     return NextResponse.redirect(new URL("/docs", request.url))
   }
@@ -24,10 +29,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/docs/:path*",
-    "/login/:path*",
-    "/signup/:path*",
-    "/signup-verification/:path*",
-  ],
+  matcher: ["/docs/:path*", "/login", "/signup", "/forgot-password", "/reset-password"],
 }
