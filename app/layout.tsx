444ccda112df18e585127b3d76183efb26356438
fsaythanry <aldyzkobf@gmail.com>
2023-11-23 16:54:27 +0800

    add sonner package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Toaster } from "sonner"
 import "./globals.css"
 
 const inter = Inter({
@@ -15,7 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-50`}>{children}</body>
+      <body className={`${inter.className} bg-zinc-50`}>
+        {children}
+
+        <Toaster />
+      </body>
     </html>
   )
 }
