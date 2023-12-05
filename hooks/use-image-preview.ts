27c0e13d13beeb8ed829fc40295325b330e5b71d
fsaythanry <aldyzkobf@gmail.com>
2023-12-05 18:57:56 +0800

    add image preview hook

---
+++ hooks/use-image-preview.ts
@@ -0,0 +1,23 @@
+import { useRef, useState } from "react"
+import { useUpdateEffect } from "usehooks-ts"
+
+export const useImagePreview = (file: File | null) => {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  useUpdateEffect(() => {
+    let url: string | null = null
+
+    if (file && file instanceof File) {
+      url = URL.createObjectURL(file)
+      setPreview(url)
+    } else {
+      setPreview(null)
+    }
+
+    return () => {
+      if (url) URL.revokeObjectURL(url)
+    }
+  }, [file])
+
+  return { preview }
+}
