7ea14635bd09a3f34007cc61b995c4adb01049c3
fsaythanry <aldyzkobf@gmail.com>
2023-12-20 23:34:22 +0800

    modify welcome page

--- app/(welcome)/page.tsx
+++ app/(welcome)/page.tsx
@@ -1,21 +1,40 @@
 import Logo from "@/components/logo"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
-import { Suspense } from "react"
 import HeroesAuth from "./_components/heroes-auth"
 import NavAuth from "./_components/nav-auth"
+import { cookies } from "next/headers"
+import { createClient } from "@/lib/supabase/server"
+
+type Profile = {
+  username: string | null
+  fullname: string | null
+}
 
 export const revalidate = 0
 
-export default function WelcomePage() {
+export default async function WelcomePage() {
+  const cookiesStore = cookies()
+  const server = createClient(cookiesStore)
+  const { data } = await server.auth.getUser()
+
+  const profile: Profile = { fullname: null, username: null }
+  if (data.user) {
+    const { data } = await server.from("profiles").select(`username, fullname`).single()
+    profile.fullname = data?.fullname ?? null
+    profile.username = data?.username ?? null
+  }
+
   return (
     <div className="mx-auto flex min-h-screen flex-col justify-between text-zinc-800">
       <nav className={"flex w-full items-center justify-between bg-background p-5"}>
         <Logo />
 
-        <Suspense fallback={<NavAuth.Loading />}>
-          <NavAuth />
-        </Suspense>
+        <NavAuth
+          email={data.user?.email ?? null}
+          fullname={profile.fullname}
+          username={profile.username}
+        />
       </nav>
 
       <main className="w-100 flex flex-col items-center justify-center">
@@ -30,9 +49,7 @@ export default function WelcomePage() {
             Station is the connected workspace where <br /> better, faster work happens.
           </p>
 
-          <Suspense fallback={<HeroesAuth.Loading />}>
-            <HeroesAuth />
-          </Suspense>
+          <HeroesAuth email={data.user?.email ?? null} />
         </div>
 
         <div className="relative mx-auto flex w-full max-w-3xl items-center justify-center overflow-hidden pl-5">
