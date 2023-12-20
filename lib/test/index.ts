a7302ccb56f76c3909675801afb4afe3ac0ca527
fsaythanry <aldyzkobf@gmail.com>
2023-12-20 22:59:19 +0800

    update vitest config

--- lib/test/index.ts
+++ lib/test/index.ts
@@ -1,6 +1,10 @@
 import { afterEach } from "vitest"
 import { cleanup } from "@testing-library/react"
 import "@testing-library/jest-dom/vitest"
+import { loadEnvConfig } from "@next/env"
+
+// load env.test.local or env.test
+loadEnvConfig(process.cwd())
 
 afterEach(() => {
   cleanup()
