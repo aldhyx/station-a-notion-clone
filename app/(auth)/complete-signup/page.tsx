6aaedc1f2fc6afc5ff0d0027e7eacee51df746c4
fsaythanry <aldyzkobf@gmail.com>
2023-12-22 20:00:45 +0800

    add rule for complete profile

--- app/(auth)/complete-signup/page.tsx
+++ app/(auth)/complete-signup/page.tsx
@@ -1,80 +1,25 @@
-"use client"
-
-import ErrorBlock from "@/components/error-block"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { LoaderIcon } from "lucide-react"
 import React from "react"
-import { useCompleteProfile } from "./_hooks/use-complete-profile"
+import { cookies } from "next/headers"
+import { createClient } from "@/lib/supabase/server"
+import { redirect } from "next/navigation"
+import FullScreenLoading from "@/components/full-screen-loading"
+import dynamic from "next/dynamic"
 
-export default function CompleteSignUpPage() {
-  const { errors, form, isDisableSubmit, isLoadingSubmit, submitHandler } =
-    useCompleteProfile()
+const CompleteSignUpPage = dynamic(() => import("./complete-signup"), {
+  loading: () => <FullScreenLoading />,
+  ssr: false,
+})
 
-  return (
-    <>
-      <h1 className="mb-3 w-full text-left text-2xl font-bold md:text-3xl">
-        Complete your profile
-      </h1>
-      <p className="mb-8 w-full  text-left">
-        Please, fill this form down below to continue using{" "}
-        <span className="font-bold underline">Station.</span>
-      </p>
+export default async function CompleteSignUpRootPage() {
+  const cookiesStore = cookies()
+  const server = createClient(cookiesStore)
 
-      <Form {...form}>
-        <form
-          onSubmit={submitHandler}
-          className="flex w-full flex-col gap-y-3"
-          autoComplete="off"
-        >
-          <FormField
-            control={form.control}
-            name="fullname"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your name..." type="text" {...field} />
-                </FormControl>
+  const { data: profile } = await server
+    .from("profiles")
+    .select("username, fullname")
+    .single()
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+  if (profile?.fullname && profile?.username) return redirect("/doc")
 
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem className="mb-2">
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your username..." type="text" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <ErrorBlock className="m-0" message={errors.root?.apiError.message} />
-
-          <Button size="lg" className="w-full" type="submit" disabled={isDisableSubmit}>
-            {isLoadingSubmit && (
-              <LoaderIcon className="animate mr-2 h-4 w-4 animate-spin" />
-            )}
-            Submit
-          </Button>
-        </form>
-      </Form>
-    </>
-  )
+  return <CompleteSignUpPage />
 }
