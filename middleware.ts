b632aca64fb2920e79b0fdfea237bb822ac3d37d
fsaythanry <aldyzkobf@gmail.com>
2023-12-22 19:53:51 +0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,23 @@
 import { NextResponse, type NextRequest } from "next/server"
 import { createClient } from "./lib/supabase/middleware"
 
+const protectedRoutes = ["/doc", "/reset-password", "/settings"]
+const privateRoutes = ["/login", "/signup", "/forget-password"]
+
 export async function middleware(request: NextRequest) {
   const { supabase, response } = createClient(request)
 
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  const { data, error } = await supabase.auth.getSession()
+  const { data } = await supabase.auth.getSession()
 
-  if (
-    !data.session &&
-    (request.nextUrl.pathname.startsWith("/doc") ||
-      request.nextUrl.pathname.startsWith("/reset-password") ||
-      request.nextUrl.pathname.startsWith("/setting"))
-  ) {
+  const { pathname } = request.nextUrl
+
+  if (!data.session && protectedRoutes.some(r => pathname.startsWith(r))) {
     return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_APP_URL))
   }
 
-  if (
-    data.session &&
-    (request.nextUrl.pathname.startsWith("/login") ||
-      request.nextUrl.pathname.startsWith("/signup") ||
-      request.nextUrl.pathname.startsWith("/forgot-password"))
-  ) {
+  if (data.session && privateRoutes.some(r => pathname.startsWith(r))) {
     return NextResponse.redirect(new URL("/doc", process.env.NEXT_PUBLIC_APP_URL))
   }
 
@@ -31,11 +26,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/doc/:path*",
-    "/login",
-    "/signup",
-    "/forgot-password",
-    "/reset-password",
-    "/settings/:path*",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 }
