19fedb8e85579bd9a12b5fe87718a51859784db9
fsaythanry <aldyzkobf@gmail.com>
2024-01-06 17:14:29 +0800

    chore: update metadata

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,15 +1,60 @@
 import type { Metadata } from "next"
 
-export const constructMetadata = (
-  opt?: {
-    title?: string
-    description?: string
-    image?: string
-    noIndex?: boolean
-  } & Metadata,
-): Metadata => ({
-  title: opt?.title || "Station note",
-  description:
-    opt?.description ||
-    "Station note app is realtime, free & open source note taking app",
+export const constructMetadata = ({
+  title = "Station note",
+  description = "Station note app is a realtime, free & open source note taking app",
+  image = "https://station-proto.netlify.app/_static/thumbnail.png", // todo: not yet
+  icons = [
+    {
+      rel: "apple-touch-icon",
+      sizes: "32x32",
+      url: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+  ],
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string | URL
+  icons?: Metadata["icons"]
+  noIndex?: boolean
+} = {}): Metadata => ({
+  title,
+  description,
+  icons,
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: image,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    images: [image],
+    creator: "@station_note",
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+  ...(noIndex && {
+    robots: {
+      index: false,
+      follow: false,
+    },
+  }),
 })
