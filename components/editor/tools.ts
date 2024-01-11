53757934cd99aa0a17d9925ffca0cb01d6f35e2f
fsaythanry <aldyzkobf@gmail.com>
2024-01-11 14:16:55 +0800

    feat: add inline, marker, nested list

--- components/editor/tools.ts
+++ components/editor/tools.ts
@@ -1,10 +1,30 @@
 import { ToolConstructable, ToolSettings } from "@editorjs/editorjs"
 import { Heading } from "./block-tool/"
-
+// @ts-ignore
+import NestedList from "@editorjs/nested-list"
+// @ts-ignore
+import Marker from "@editorjs/marker"
+// @ts-ignore
+import InlineCode from "@editorjs/inline-code"
 export const tools: Record<string, ToolConstructable | ToolSettings> = {
   heading: {
     //@ts-ignore
     class: Heading,
     inlineToolbar: ["italic"],
+  },
+  list: {
+    class: NestedList,
+    inlineToolbar: true,
+    config: {
+      defaultStyle: "unordered",
+    },
+  },
+  marker: {
+    class: Marker,
+    shortcut: "CMD+M",
+  },
+  inlineCode: {
+    class: InlineCode,
+    shortcut: "CMD+E",
   },
 }
