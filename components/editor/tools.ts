7f10cca1a7d4d0f38842db0055ce112a72db7984
fsaythanry <aldyzkobf@gmail.com>
2024-01-15 00:24:56 +0800

    feat: use alignment blocktune

--- components/editor/tools.ts
+++ components/editor/tools.ts
@@ -1,4 +1,8 @@
-import { ToolConstructable, ToolSettings } from "@editorjs/editorjs"
+import {
+  type BlockToolConstructable,
+  type EditorConfig,
+  type ToolConfig,
+} from "@editorjs/editorjs"
 // @ts-ignore
 import NestedList from "@editorjs/nested-list"
 // @ts-ignore
@@ -7,20 +11,22 @@ import Marker from "@editorjs/marker"
 import InlineCode from "@editorjs/inline-code"
 
 import { HeadingBlock, ParagraphBlock } from "./block-tool/"
+import { type Paragraph, type Heading } from "./block-tool/index.type"
+import { AlignmentTune } from "./block-tune"
 
-export const tools: Record<string, ToolConstructable | ToolSettings> = {
+export const tools: EditorConfig["tools"] = {
+  alignment: AlignmentTune,
   paragraph: {
-    //@ts-ignore
-    class: ParagraphBlock,
+    class: ParagraphBlock as unknown as BlockToolConstructable,
     inlineToolbar: true,
+    config: { preserveBlank: true } as ToolConfig<Paragraph["Config"]>,
+    tunes: ["alignment"],
   },
   heading: {
-    //@ts-ignore
-    class: HeadingBlock,
+    class: HeadingBlock as unknown as BlockToolConstructable,
     inlineToolbar: ["italic"],
-    config: {
-      defaultLevel: 2,
-    },
+    config: { defaultLevel: 2 } as ToolConfig<Heading["Config"]>,
+    tunes: ["alignment"],
   },
   nestedList: {
     class: NestedList,
